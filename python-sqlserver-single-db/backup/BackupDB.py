17ecf0b509c6bedd8eaf090b1633b87eb03893db
Navpreet14-02 <naviubhi123@gmail.com>
2024-07-30 15:54:03 +0530

    Updated Backup Functionality

--- python-sqlserver-single-db/backup/BackupDB.py
+++ python-sqlserver-single-db/backup/BackupDB.py
@@ -3,24 +3,24 @@ from datetime import date
 
 def BackupDB(server_name,database,username,password):
     connection_string = f'Driver={{SQL Server}};Server={server_name};Database={database};Uid={username};Pwd={password};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;'
-    try:
-        print("Backup Function Executing:")
-        with pyodbc.connect(connection_string) as conn:
-            cursor=conn.cursor()
-            today = date.today()
-            backup_query = f"SELECT * FROM {database} INTO {database+f"-{date.isoformat(today)}"}"
+    # try:
+    print("Backup Function Executing:")
+    with pyodbc.connect(connection_string) as conn:
+        cursor=conn.cursor()
+        today = date.today()
+        backup_query = f"SELECT * FROM {database} INTO {database+f"-{date.isoformat(today)}"}"
 
-            try:
-                cursor.execute(backup_query)
-                conn.commit()
-            except pyodbc.Error as err:
-                print(f"Error while creating backup: {err}")
-                quit()
+        # try:
+        cursor.execute(backup_query)
+        conn.commit()
+        # except pyodbc.Error as err:
+        #     print(f"Error while creating backup: {err}")
+        #     quit()
 
-            print(backup_query)
+        print(backup_query)
 
-    except pyodbc.Error as err:
-        print(f"Database Connection could not be established: {err}")
+    # except pyodbc.Error as err:
+    #     print(f"Database Connection could not be established: {err}")
 
 
 
